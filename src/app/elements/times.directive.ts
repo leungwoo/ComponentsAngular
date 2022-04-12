e177c9ed645e24e05794c63bcd41f2bf28b52552
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-12 01:13:06 -0400

    Add custom directive to edit lines in placeholder

---
+++ src/app/elements/times.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ViewContainerRef, TemplateRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appTimes]',
+})
+export class TimesDirective {
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef
+  ) {}
+
+  @Input('appTimes') set render(times: number) {
+    this.viewContainerRef.clear();
+    for (let i = 0; i < times; ++i) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef, {});
+    }
+  }
+}
