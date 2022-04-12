93f5bbf416f26c87d68774941a6622a201782182
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-12 15:33:49 -0400

    Segment child  component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
