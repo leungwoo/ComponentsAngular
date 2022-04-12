3ec252667157f33e7abcb1194d9fe9fa6158ec80
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-12 01:57:55 -0400

    ng content parent to child component sharing

--- src/app/shared/divider/divider.component.ts
+++ src/app/shared/divider/divider.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-divider',
   templateUrl: './divider.component.html',
-  styleUrls: ['./divider.component.css']
+  styleUrls: ['./divider.component.css'],
 })
 export class DividerComponent implements OnInit {
+  @Input() title = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
