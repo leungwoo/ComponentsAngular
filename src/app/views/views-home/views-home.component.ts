a7ffdc231dc06b38979718d5e437ad14083b745f
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-13 13:57:37 -0400

    statistics component child

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.css']
+  styleUrls: ['./views-home.component.css'],
 })
 export class ViewsHomeComponent implements OnInit {
+  stats = [
+    { value: 22, label: '# of users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'reviews' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
