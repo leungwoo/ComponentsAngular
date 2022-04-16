7306f4a25b32d94bc79cb1c27a59f81320f0a7aa
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-16 16:47:32 -0400

    Accordion setup

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
+  items = [
+    {
+      title: 'Why is the sky blue',
+      content: 'The sky is bkue becasue it is',
+    },
+    {
+      title: 'What is  your fav food',
+      content: 'None i eat everything',
+    },
+    {
+      title: 'Why is it so hot',
+      content: 'There is no breeze',
+    },
+  ];
   openModal = false;
   constructor() {}
 
