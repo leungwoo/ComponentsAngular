841afb59dfc526dca2850c58f395bd4219975110
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-16 16:06:32 -0400

    Reusable Modal

---
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mods-home',
+  templateUrl: './mods-home.component.html',
+  styleUrls: ['./mods-home.component.css'],
+})
+export class ModsHomeComponent implements OnInit {
+  openModal = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+  onClick() {
+    this.openModal = !this.openModal;
+  }
+}
